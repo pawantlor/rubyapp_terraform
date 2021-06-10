24b6fb0eb7a9529116785ac12ac5f828acdb36c1
pawantlor <pawantlor@gmail.com>
2021-06-10 23:22:58 +0530

    Add files via upload
    Updated ci-cd.sh file with parameters, also comments.

--- ci-cd.sh
+++ ci-cd.sh
@@ -1,36 +1,50 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+        echo "Local docker repo is not Provided, Please provide local docker repo! Exiting"
+        exit 1
+fi
 localrepo=$1
-echo "Starting with hosting of rubyapp Using Shell,Docker,Terraform"
+echo "*****Starting with hosting of rubyapp Using Shell,Docker,Terraform*****"
 var1="test-assignment"
 mkdir -p $var1
 cd $var1
-git clone https://github.com/pawantlor/ruby-app.git
-cd ruby-app
+echo "*****Git clone of repo done*****"
+git clone https://github.com/pawantlor/rubyapp_terraform.git
+cd rubyapp_terraform
 if [ ! -f Dockerfile ]; then
 	echo "Could not find Dockerfile! Exiting"
 	exit 1
+else
+        echo "*****Found Dockerfile! Proceeding*****"
 fi
 
 if [ ! -f http_server.rb ]; then
         echo "Could not find application file! Exiting"
         exit 1
+else
+        echo "*****Found Application file! Proceeding*****"
 fi
 
-docker build -t $localrepo/rubyapp:1.16 .
-docker push $localrepo/rubyapp:1.16
+echo "*****Running Docker Build*****"
+docker build -t $localrepo/rubyapp:1.0 .
+docker push $localrepo/rubyapp:1.0
+echo "*****Running Docker Build*****"
 
 
 if [ ! -f main.tf ]; then
         echo "Wont be able to exexcute terraform! Exiting"
         exit 1
+else
+        echo "*****Found Terraform main file! Proceeding*****"
 fi
 
 echo "*********** Terraform Init*********"
 terraform init
 echo "*********** Terraform Init*********"
-terraform plan 
+terraform plan -var "image-name=$localrepo/rubyapp:1.0" -out=tfplan
 
 echo "*********** Terraform Apply*********"
-terraform apply -var "image-name=$localrepo/rubyapp:1.16"
+
+terraform apply -auto-approve "tfplan"
 
